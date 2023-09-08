52061c6c7cadc6ee8dfcc82e3bae4b47cc0f45af
Mkzay <80535194+Mkzay@users.noreply.github.com>
2023-09-08 09:13:42 +0100

    millisecond

--- app.js
+++ app.js
@@ -14,19 +14,21 @@ function updateCurrentDay() {
 updateCurrentDay();
 
 //Script for Time
-function getCurrentUTCTime() {
+function getCurrentUTCTimeWithMilliseconds() {
   const now = new Date();
   const hours = now.getUTCHours().toString().padStart(2, '0');
   const minutes = now.getUTCMinutes().toString().padStart(2, '0');
   const seconds = now.getUTCSeconds().toString().padStart(2, '0');
-  return `${hours}:${minutes}:${seconds}`;
+  const milliseconds = now.getUTCMilliseconds().toString().padStart(3, '0');
+  return `${hours}:${minutes}:${seconds}.${milliseconds}`;
 }
 
-const currentUTCTime = getCurrentUTCTime();
+const currentUTCTimeWithMilliseconds = getCurrentUTCTimeWithMilliseconds();
 const currentUTCTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
 
 if (currentUTCTimeElement) {
-  currentUTCTimeElement.textContent = currentUTCTime;
+  currentUTCTimeElement.textContent = currentUTCTimeWithMilliseconds;
 }
+
 
   
