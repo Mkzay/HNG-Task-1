a4f4519503ba637c0c3fd26c0ab3472e14ddbae2
Mkzay <80535194+Mkzay@users.noreply.github.com>
2023-09-08 22:33:37 +0100

    changes

--- app.js
+++ app.js
@@ -1,34 +1,14 @@
-//Script for Day
-function updateCurrentDay() {
-    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const now = new Date();
-    const dayOfWeek = daysOfWeek[now.getDay()];
+document.addEventListener("DOMContentLoaded", function () {
+  const dayOfWeekElement = document.getElementById("dayOfWeek");
+  const utcTimeElement = document.getElementById("utcTime");
 
-    const currentDay = document.querySelector('[data-testid="currentDayOfTheWeek"]');
-    
-    if (currentDay) {
-        currentDay.textContent = dayOfWeek;
-    }
-}
+  //Day of the week
+  const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const currentDate = new Date();
+  const dayOfWeek = daysOfWeek[currentDate.getUTCDay()];
+  dayOfWeekElement.textContent = dayOfWeek;
 
-updateCurrentDay();
-
-//Script for Time
-function getCurrentUTCTimeWithMilliseconds() {
-  const now = new Date();
-  const hours = now.getUTCHours().toString().padStart(2, '0');
-  const minutes = now.getUTCMinutes().toString().padStart(2, '0');
-  const seconds = now.getUTCSeconds().toString().padStart(2, '0');
-  const milliseconds = now.getUTCMilliseconds().toString().padStart(3, '0');
-  return `${hours}:${minutes}:${seconds}.${milliseconds}`;
-}
-
-const currentUTCTimeWithMilliseconds = getCurrentUTCTimeWithMilliseconds();
-const currentUTCTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
-
-if (currentUTCTimeElement) {
-  currentUTCTimeElement.textContent = currentUTCTimeWithMilliseconds;
-}
-
-
-  
+  //UTC time in milliseconds
+  const utcTime = currentDate.getTime();
+  utcTimeElement.textContent = utcTime;
+});
